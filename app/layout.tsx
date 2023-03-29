42d12d592ba487e06da89be4352561d40aa82b86
DESKTOP-SCBAE9E\Daniel <dan08jan@gmail.com>
2023-03-29 19:32:57 -0300

    Context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import '../styles/globals.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import { StateContext } from '../context/StateContext';
 import { Toaster } from 'react-hot-toast';
 
