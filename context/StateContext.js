08c9fb25e88b45095173e60116b46a501491e2cd
DESKTOP-SCBAE9E\Daniel <dan08jan@gmail.com>
2023-03-29 18:31:13 -0300

    Creating context

--- context/StateContext.js
+++ context/StateContext.js
@@ -4,7 +4,26 @@ import { toast } from 'react-hot-toast';
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
-  
+  const [showCart, setShowCart] = useState(false);
+  const [cartItens, setCartItens] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuantities, setTotalQuantities] = useState();
+  const [qty, setQty] = useState(1)
+
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItens,
+        totalPrice,
+        totalQuantities,
+        qty
+      }}
+    >
+      {children}
+    </Context.Provider>
+  )
+
 }
 
 
